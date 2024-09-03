dbcd275847e31f2fa7412e8dda2ac7d5d7e70fb2
yutian81 <165384851+yutian81@users.noreply.github.com>
2024-09-03 12:55:01 +0800

    Update _workers3.js

--- CFcdn-proxy/_workers3.js
+++ CFcdn-proxy/_workers3.js
@@ -12,156 +12,150 @@ async function handleRequest(request) {
 
 	if (!actualUrlStr) {
 		const mainDomain = url.hostname;
-		const websiteTitle = "Anti proxy for Mingyu"; // 请替换为您的网站标题
+		const websiteTitle = "在线代理服务器"; // 请替换为您的网站标题
 		const errorMessage = `
-		<html>
-		    <head>
-		      <title>${websiteTitle}</title>
-			  <link rel="icon" type="image/jpg"
-			href="https://cdn.jsdelivr.net/gh/png-dot/pngpng@main/20231112-014821-y4poc8.jpg">
-			  <meta charset="UTF-8">
-		      <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no, minimal-ui" />
-		      <meta name="apple-mobile-web-app-capable" content="yes">
-		      <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
-		      <style>
+		<!DOCTYPE html>
+		<html lang="zh">
+		<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta name="apple-mobile-web-app-capable" content="yes">
+		<meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
+		<title>${websiteTitle}</title>
+		<link rel="icon" type="image/jpg" href="https://cdn.jsdelivr.net/gh/png-dot/pngpng@main/20231112-014821-y4poc8.jpg">
+		<style>
 			body {
-				font-family: Arial, sans-serif;
-				text-align: center;
-				background-color: #f0e6fa;
-				margin: 0;
-				padding: 0;
+			font-family: Arial, sans-serif;
+			text-align: center;
+			background-color: #f0e6fa;
+			margin: 0;
+			padding: 0;
+			display: flex;
+			align-items: center;
+			justify-content: center;
+			height: 100vh;
 			}
 
 			#container {
-				max-width: auto;
-				margin: 0 auto;
-				background-color: #fff;
-				padding: 20px;
-				border-radius: 10px;
-				box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+			max-width: 85%;
+			width: 100%;
+			background-color: #fff;
+			padding: 20px;
+			border-radius: 10px;
+			box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+			box-sizing: border-box;
+			}
+
+			h1 {
+			margin: 0;
+			font-size: 2em;
+			color: #333;
+			}
+
+			.form-group {
+			margin-top: 20px;
+			}
+
+			label {
+			display: block;
+			margin-bottom: 8px;
+			font-size: 1.1em;
+			color: #333;
 			}
 
 			input[type="text"] {
-				width: 100%;
-				padding: 8px;
-				border: 1px solid #ccc;
-				border-radius: 3px;
-				box-sizing: border-box;
-				display: block;
-				margin-top: 10px;
-				margin-bottom: 10px;
+			width: 90%;
+			padding: 10px;
+			border: 1px solid #ccc;
+			border-radius: 5px;
+			box-sizing: border-box;
+			display: inline-block;
+			margin: 10px auto;
+			margin-top: 10px;
+			margin-bottom: 20px;	
 			}
 
 			input[type="button"] {
-				background-color: #ff6b81;
-				color: #fff;
-				border: none;
-				padding: 10px 0;
-				border-radius: 5px;
-				cursor: pointer;
-				transition: background-color 0.1s ease;
-				width: 100%;
+			background-color: #ff6b81;
+			color: #fff;
+			border: none;
+			padding: 12px;
+			border-radius: 5px;
+			cursor: pointer;
+			transition: background-color 0.3s ease;
+			width: 90%;
+			font-size: 1.1em;
 			}
 
 			input[type="button"]:hover {
-				background-color: #2980b9;
+			background-color: #ff4757;
 			}
 
 			@keyframes shake {
-				0% {
-					transform: translateX(0);
-				}
-
-				25% {
-					transform: translateX(-5px);
-				}
-
-				50% {
-					transform: translateX(5px);
-				}
-
-				75% {
-					transform: translateX(-5px);
-				}
-
-				100% {
-					transform: translateX(5px);
-				}
+			0% { transform: translateX(0); }
+			25% { transform: translateX(-5px); }
+			50% { transform: translateX(5px); }
+			75% { transform: translateX(-5px); }
+			100% { transform: translateX(0); }
 			}
 
-			@media (min-width: 768px) {
-				body {
-					display: flex;
-					align-items: center;
-					justify-content: center;
-					height: 100vh;
-					/* 设置body的高度为视口高度，使其垂直方向上居中 */
-				}
-
-				#container {
-					width: 100%;
-				}
-			}
-
-			/* 暗黑模式 */
 			@media (prefers-color-scheme: dark) {
-				body {
-					background-color: #333;
-				}
+			body {
+				background-color: #333;
+			}
 
-				#container {
-					background-color: #CDC1C5;
-				}
+			#container {
+				background-color: #444;
+				color: #ddd;
+			}
+
+			input[type="text"], input[type="button"] {
+				color: #ddd;
+			}
 			}
 		</style>
-		    </head>
-		    <body>
-		      <div id="container">
-		        <h1>${websiteTitle}</h1>
-		        <div class="form-group">
-		          <label for="url">输入需要代理的网站:</label>
-		          <input type="text" id="url" name="url" placeholder="例如：https://github.com/" />
-		          <input type="button" id="submit" value="进入代理" onclick="redirectToProxy()" />
-		        </div>
-		  <p>&copy; 2023 <a href="https://github.com/ymyuuu/" target="_blank">Mingyu</a></p>
-		      </div>
-		      <script>
-		        function redirectToProxy() {
-		          var urlInput = document.getElementById('url');
-		          var inputUrl = urlInput.value.trim(); // 移除前后空格
-		          if (inputUrl) {
-		            var url = normalizeUrl(inputUrl);
-		            window.open('https://' + '${mainDomain}' + '/' + url, '_blank');
-		            // 清空输入框内容
-		            urlInput.value = '';
-		          } else {
-		            // 如果没有输入URL，执行抖动效果
-		            urlInput.style.animation = 'shake 0.5s';
-		            setTimeout(() => {
-		              urlInput.style.animation = ''; // 清除抖动效果
-		            }, 500);
-		          }
-		        }
-		
-		        function normalizeUrl(inputUrl) {
-		          // 检查输入的URL是否以 "http://" 或 "https://" 开头
-		          if (!inputUrl.startsWith("http://") && !inputUrl.startsWith("https://")) {
-		            // 如果不是以 "http://" 或 "https://" 开头，则默认添加 "https://"
-		            inputUrl = "https://" + inputUrl;
-		          }
-		          return inputUrl;
-		        }
-		
-		        // 添加键盘事件监听器
-		        document.addEventListener('keydown', function(event) {
-		          if (event.key === 'Enter') {
-		            // 如果按下回车键，触发提交按钮的点击事件
-		            submit.click();
-		          }
-		        });
-		      </script>
-		    </body>
-		    </html>
+		</head>
+		<body>
+		<div id="container">
+			<h1>${websiteTitle}</h1>
+			<div class="form-group">
+			<label for="url">输入需要代理的网站</label>
+			<input type="text" id="url" name="url" placeholder="例如：https://github.com/" />
+			<input type="button" id="submit" value="进入代理" onclick="redirectToProxy()" />
+			</div>
+			<p>&copy; 2024 <a href="https://github.com/yutian81/" target="_blank">yutian81</a></p>
+		</div>
+		<script>
+			function redirectToProxy() {
+			var urlInput = document.getElementById('url');
+			var inputUrl = urlInput.value.trim(); // 移除前后空格
+			if (inputUrl) {
+				var url = normalizeUrl(inputUrl);
+				window.open('https://' + '${mainDomain}' + '/' + url, '_blank');
+				urlInput.value = ''; // 清空输入框内容
+			} else {
+				urlInput.style.animation = 'shake 0.5s';
+				setTimeout(() => {
+				urlInput.style.animation = ''; // 清除抖动效果
+				}, 500);
+			}
+			}
+
+			function normalizeUrl(inputUrl) {
+			if (!inputUrl.startsWith("http://") && !inputUrl.startsWith("https://")) {
+				inputUrl = "https://" + inputUrl;
+			}
+			return inputUrl;
+			}
+
+			document.addEventListener('keydown', function(event) {
+			if (event.key === 'Enter') {
+				document.getElementById('submit').click();
+			}
+			});
+		</script>
+		</body>
+		</html>
 	  `;
 
 		return new Response(errorMessage, {
