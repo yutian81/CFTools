8af945987f1044524b1bc4406eedcc230b5c170b
yutian81 <165384851+yutian81@users.noreply.github.com>
2024-09-29 10:04:38 +0800

    Update _workers2.js

--- CFcdn-proxy/_workers2.js
+++ CFcdn-proxy/_workers2.js
@@ -13,10 +13,9 @@ export default {
 export default {
   async fetch(request, env) {
     let url = new URL(request.url);
-    // 目标网址的域名
-    url.hostname = 'cdn.cloudflare.steamstatic.com'; // 设置需要反代的地址
+    // 目标网址的域名、协议和路径
+    url.hostname = 'cdn.cloudflare.steamstatic.com';
     url.protocol = "https";
-    // 目标网址的路径
     url.pathname = '/steam/';
     return fetch(new Request(url, request));
   },
