c9aa014e234afd8eb242656c844186137ac7cab5
yutian81 <165384851+yutian81@users.noreply.github.com>
2024-09-29 10:01:47 +0800

    Update _workers2.js

--- CFcdn-proxy/_workers2.js
+++ CFcdn-proxy/_workers2.js
@@ -1,17 +1,24 @@
-addEventListener('fetch', event => {
-    event.respondWith(handleRequest(event.request));
-});
-
-async function handleRequest(request) {
+// 方案1-反代域名-简版(可以page)
+export default {
+  async fetch(request, env) {
     let url = new URL(request.url);
-    
-    // 设置目标主机名和协议
-    url.hostname = "yourdomain.com"; // 复制你的域名
+    url.hostname = 'api.cbcd.com'; // 设置需要反代的地址
     url.protocol = "https";
-    
-    // 创建一个新的请求对象，保持原请求的所有参数
-    let newRequest = new Request(url, request);
-    
-    // 发送请求并返回响应
-    return fetch(newRequest);
-}
+    return fetch(new Request(url, request));
+  },
+};
+
+//----------------------------------------------------------------------------
+// 方案2-反代伪装域名下某个路径(可以Page)
+export default {
+  async fetch(request, env) {
+    let url = new URL(request.url);
+    // 目标网址的域名
+    url.hostname = 'cdn.cloudflare.steamstatic.com'; // 设置需要反代的地址
+    url.protocol = "https";
+    // 目标网址的路径
+    url.pathname = '/steam/';
+    return fetch(new Request(url, request));
+  },
+};
+
