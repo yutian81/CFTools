75b03dc6499ae485e61ceb4c361d0ea185d1db24
yutian81 <yutian88881@hotmail.com>
2024-11-19 22:55:14 +0800

    Update _worker.js

--- ASN-CIDR/_worker.js
+++ ASN-CIDR/_worker.js
@@ -1,53 +1,46 @@
-// 定义一个函数，用来从HTML字符串中提取IPv4的CIDR
-function extractIPv4CIDRs(html) {
-  // 创建一个空的Set对象，用来存放不重复的CIDR
-  let result = new Set();
-  // 创建一个正则表达式，用来匹配IPv4的CIDR
-  let regex = /(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\/\d{1,2})/g;
-  // 使用正则表达式在HTML字符串中查找所有的匹配项
-  let match = regex.exec(html);
-  // 循环遍历所有的匹配项
-  while (match) {
-    // 把匹配项的第一个分组（即IPv4的CIDR）添加到Set对象中
-    result.add(match[1]);
-    // 继续查找下一个匹配项
-    match = regex.exec(html);
-  }
-  // 返回Set对象转换成的数组
-  return [...result];
-}
+let GH_NAME = 'ipverse';
 
-// 定义一个函数，用来处理请求
-async function handleRequest(request) {
-  // 获取请求的URL
-  let url = new URL(request.url);
-  // 获取URL后面的ASN号
-  let asn = url.pathname.slice(1);
-  // 如果ASN号不为空
-  if (asn) {
-    // 拼接一个新的URL，用来访问asnlookup.com
-    let newUrl = "https://asnlookup.com/asn/" + asn;
-    // 发送一个GET请求，获取响应内容
-    let response = await fetch(newUrl);
-    let html = await response.text();
-    // 从响应内容中提取IPv4的CIDR
-    let cidrs = extractIPv4CIDRs(html);
-    // 把结果数组转换成字符串，每个元素占一行
-    let output = cidrs.join("\n");
-    // 返回一个新的响应，包含结果字符串
-    return new Response(output, {
-      headers: { "content-type": "text/plain" },
-    });
-  } else {
-    // 如果ASN号为空，返回一个错误信息
-    return new Response("Please provide an ASN number", {
-      status: 400,
-      statusText: "Bad Request",
-    });
+export default {
+  async fetch (request,env) {
+    const url = new URL(request.url);
+    const path = url.pathname;
+
+    if (path === '/') return new Response( request.headers.get('cf-connecting-ip'), { status: 200 });
+
+    const match = path.split('.json')[0].match(/(\d{1,6})/);
+    let ASN = '45102';
+    if (match) ASN = match[1];
+    else return new Response('"无效的 ASN:1"', { status: 400 });
+
+    GH_NAME = env.GH_NAME || GH_NAME;
+    const ASN_URL = `https://raw.githubusercontent.com/ipverse/asn-ip/refs/heads/master/as/${ASN}/aggregated.json`;
+    const response = await fetch(ASN_URL);
+    if (!response.ok) return new Response('"无效的 ASN:2"', { status: 400 });
+    
+    const data = await response.json();
+    console.log(data);
+    
+    if (path.endsWith('.json')) {
+      return new Response(JSON.stringify(data, null, 4), {
+        status: 200,
+        headers: {
+          'content-type': 'application/json',
+        },
+      });
+    } else {
+      const ipv4Subnets = data.subnets.ipv4;
+      const ipv4Text = ipv4Subnets.join('\n');
+      let text = ipv4Text;
+      if( (url.searchParams.has('6') && url.searchParams.has('4')) || url.searchParams.has('all') ){
+        const ipv6Subnets = data.subnets.ipv6;
+        const ipv6Text = ipv6Subnets.join('\n');
+        if (ipv6Text) text += '\n' + ipv6Text;
+      } else if( url.searchParams.has('6') ){
+        const ipv6Subnets = data.subnets.ipv6;
+        const ipv6Text = ipv6Subnets.join('\n');
+        text = ipv6Text;
+      }
+      return new Response(text, { status: 200 });
+    }
   }
 }
-
-// 监听请求事件，调用处理函数
-addEventListener("fetch", (event) => {
-  event.respondWith(handleRequest(event.request));
-});
